829373b8fd471bbe21a2e510494fb450261d0c81
Cameron Arshadi <arshadic23@gmail.com>
2022-06-27 16:44:55 -0700

    replace jpype JException with specific java exception

--- imgutil.py
+++ imgutil.py
@@ -1,15 +1,15 @@
-from jpype import JException
-
+import jpype.imports
 
 # FIXME this would probably be a lot faster within Java
 def local_intensities(imglib_iterable):
+    import java.lang
     cursor = imglib_iterable.cursor()
     while cursor.hasNext():
         cursor.fwd()
         try:
             yield cursor.get().get()
-        except JException as e:
-            # ArrayIndexOutOfBounds is expected here.
-            # cursors don't check whether they are in a valid
+        except java.lang.ArrayIndexOutOfBoundsException as e:
+            # ArrayIndexOutOfBoundsException is expected here.
+            # ImgLib cursors don't check whether they are in a valid
             # portion of the image when accessing pixels.
             continue
