65bd0d208a1e551caf5f6087011c90d3566b6e2e
Cameron Arshadi <arshadic23@gmail.com>
2022-08-31 19:53:45 -0700

    fix: java config on headless Linux machines

--- src/refinery/javahelpers/__init__.py
+++ src/refinery/javahelpers/__init__.py
@@ -1,16 +1,25 @@
 import logging
 
-from .config import get_fiji_version, get_snt_version
+from .config import *
 
 import scyjava.config as sjconf
 
 
 _logger = logging.getLogger(__name__)
 
-_snt_version = get_snt_version()
-_SNT_ENDPOINT = f"org.morphonets:SNT:{_snt_version}"
-sjconf.endpoints.append(_SNT_ENDPOINT)
+sjconf.add_option("-Djava.awt.headless=true")
+
+_java_opts = get_java_opts()
+if _java_opts is not None:
+    for opt in _java_opts.split(" "):
+        sjconf.add_option(opt)
 
 _fiji_version = get_fiji_version()
 _FIJI_ENDPOINT = f"sc.fiji:fiji:{_fiji_version}"
 sjconf.endpoints.append(_FIJI_ENDPOINT)
+
+_snt_version = get_snt_version()
+_SNT_ENDPOINT = f"org.morphonets:SNT:{_snt_version}"
+sjconf.endpoints.append(_SNT_ENDPOINT)
+
+sjconf.endpoints.append("net.imagej:imagej-legacy:MANAGED")
--- src/refinery/javahelpers/config.py
+++ src/refinery/javahelpers/config.py
@@ -10,6 +10,10 @@ _fiji_version = os.getenv(
     "FIJI_VERSION", _default_fiji_version
 )
 
+_default_java_opts = None
+_java_opts = os.getenv(
+    "JAVA_OPTS", _default_java_opts
+)
 
 def set_snt_version(version):
     global _snt_version
@@ -29,3 +33,8 @@ def set_fiji_version(version):
 def get_fiji_version():
     global _fiji_version
     return _fiji_version
+
+
+def get_java_opts():
+    global _java_opts
+    return _java_opts
