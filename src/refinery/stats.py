69d4c3e3da78cb60de33afd07b4f3ad8a49ccbba
Cameron Arshadi <arshadic23@gmail.com>
2022-09-26 17:20:18 -0700

    feat: add stats module

---
+++ src/refinery/stats.py
@@ -0,0 +1,38 @@
+import argparse
+import os
+
+import scyjava
+
+from refinery.util.java import snt
+
+
+def print_stats(swc_dir):
+    total_length = 0
+    total_bps = 0
+    for root, dirs, files in os.walk(swc_dir):
+        swcs = [os.path.join(root, f) for f in files if f.endswith('.swc')]
+        if not swcs:
+            continue
+        for f in swcs:
+            swc = os.path.join(root, f)
+            t = snt.Tree(swc)
+            graph = t.getGraph()
+            total_length += graph.sumEdgeWeights()
+            total_bps += len(graph.getBPs())
+    print(f"Total length: {total_length}")
+    print(f"Total branch points: {total_bps}")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', type=str, help='directory of .swc files to render')
+
+    args = parser.parse_args()
+
+    scyjava.start_jvm()
+
+    print_stats(args.input)
+
+
+if __name__ == "__main__":
+    main()
