8504c9e14596ae93e99b383edab34f5a01c4a44c
Cameron Arshadi <arshadic23@gmail.com>
2022-09-28 16:26:44 -0700

    feat: script to organize data (wip)

---
+++ src/refinery/organize_outputs.py
@@ -0,0 +1,147 @@
+import argparse
+import json
+import os
+import re
+import shutil
+from pathlib import Path
+
+import numpy as np
+import tifffile
+import zarr
+
+
+def get_tiff_shape(tiff_path):
+    with tifffile.TiffFile(tiff_path) as tif:
+        # Open as zarr-store in case we're dealing with
+        # ImageJ hyperstacks.
+        # tifffile.TiffFile is unable to parse Z-slices with ImageJ
+        # Tiffs larger than 4GB.
+        z = zarr.open(tif.aszarr(), "r")
+        return z.shape
+
+
+def copy_blocks(src: Path, dst: Path, voxel_spacing: list):
+    block_dir = src / "blocks"
+    for block in block_dir.iterdir():
+        if not block.name.startswith("block"):
+            continue
+
+        stack_dir = block / "stack"
+        stack = stack_dir / f"{block.name}.tif"
+
+        out_block = dst / block.relative_to(src)
+        out_block.mkdir(parents=True, exist_ok=True)
+
+        shutil.copyfile(stack, out_block / "input.tif")
+
+        block_metadata = {}
+
+        # TODO: refactor for block creation script change
+        origin_file = block / "origin_vx.txt"
+        with open(origin_file, 'r') as f:
+            s = f.readline()
+            s = s.replace("[", "")
+            s = s.replace("]", "")
+            o = np.fromstring(s, dtype=int, sep=' ')
+            origin = o.tolist()
+            block_metadata['chunk_origin'] = origin
+
+        block_shape = list(reversed(get_tiff_shape(stack)))
+        block_metadata['chunk_shape'] = block_shape
+
+        block_metadata['voxel_spacing'] = voxel_spacing
+
+        with open(out_block / "metadata.json", 'w') as f:
+            json.dump(block_metadata, f, indent=4)
+
+
+def find_args(d: Path):
+    for f in d.iterdir():
+        if f.name == "args.json":
+            with open(f, 'r') as jsonf:
+                return json.load(jsonf)
+    return None
+
+
+def copy_masks(src: Path, dst: Path):
+    block_pattern = re.compile(r"block_\d+")
+    mask_pattern = re.compile(r"Fill_(Gray|Label)_Mask.tif$")
+
+    blocks_dir = dst / "blocks"
+    assert blocks_dir.is_dir()
+
+    for p in src.iterdir():
+        if not p.is_dir():
+            continue
+        if "masks" not in p.name:
+            continue
+        args = find_args(p)
+        if args is None:
+            raise ValueError(f"Could not find args.json in {p}")
+
+        fill_threshold = float(args['threshold'])
+        fill_cost_function = str(args['cost'])
+
+        for f in p.iterdir():
+            m = block_pattern.search(f.name)
+            if not m:
+                continue
+            block_id = m.group(0)
+
+            m = mask_pattern.search(f.name)
+            mask_type = m.group(0)
+
+            block_dir = blocks_dir / block_id
+            shutil.copyfile(f, block_dir / mask_type)
+
+            metadata_file = block_dir / "metadata.json"
+            with open(metadata_file, 'r') as mf:
+                metadata = json.load(mf)
+
+            metadata['fill_threshold'] = fill_threshold
+            metadata['fill_cost_function'] = fill_cost_function
+            metadata['fill_method'] = "dijkstra"
+
+            with open(metadata_file, 'w') as mf:
+                json.dump(metadata, mf, indent=4)
+
+
+def copy_swcs(src: Path, dst: Path):
+    swc_dir = src / "swcs" / "final-trees"
+    assert swc_dir.is_dir()
+
+    out_swc_dir = dst / "swcs"
+    out_swc_dir.mkdir(parents=True, exist_ok=True)
+
+    for block_dir in swc_dir.iterdir():
+        if not block_dir.is_dir():
+            continue
+        block_name = block_dir.name
+
+        out_block_dir = out_swc_dir / block_name / "final-trees"
+        out_block_dir.mkdir(parents=True, exist_ok=True)
+
+        swcs = [f for f in block_dir.iterdir() if f.name.endswith(".swc")]
+        for i, swc in enumerate(swcs):
+            new_name = f"SNT_Data-{i:04}.swc"
+            shutil.copyfile(swc, out_block_dir / new_name)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", type=str)
+    parser.add_argument("output", type=str)
+    parser.add_argument("--voxel-size", type=float, nargs='+', default=[1.0, 1.0, 1.0])
+    args = parser.parse_args()
+
+    in_dir = Path(args.input)
+    out_dir = Path(args.output)
+
+    os.makedirs(out_dir, exist_ok=True)
+
+    copy_blocks(in_dir, out_dir, args.voxel_size)
+    copy_masks(in_dir, out_dir)
+    copy_swcs(in_dir, out_dir)
+
+
+main()
