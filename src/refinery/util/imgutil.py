9df861fc515b694433f9e6f2884b4e912e60b86a
Cameron Arshadi <arshadic23@gmail.com>
2022-12-08 16:06:56 -0800

    feat: hyperslice function

--- src/refinery/util/imgutil.py
+++ src/refinery/util/imgutil.py
@@ -1,6 +1,9 @@
 import jpype.imports
 
 # FIXME this would probably be a lot faster within Java
+from refinery.util.java import imglib2
+
+
 def local_intensities(imglib_iterable):
     import java.lang
 
@@ -14,3 +17,9 @@ def local_intensities(imglib_iterable):
             # ImgLib cursors don't check whether they are in a valid
             # portion of the image when accessing pixels.
             continue
+
+
+def get_hyperslice(img, ndim=3):
+    while img.numDimensions() > ndim:
+        img = imglib2.Views.hyperSlice(img, img.numDimensions()-1, 0)
+    return img
