068bf03b2e70fce57d02e212b3e898885764726c
Cameron Arshadi <arshadic23@gmail.com>
2022-12-22 22:52:05 -0800

    sub-pixel mean shift

--- src/refinery/refine.py
+++ src/refinery/refine.py
@@ -9,6 +9,7 @@ from pathlib import Path
 
 import numpy as np
 import scyjava
+from scipy.interpolate import RegularGridInterpolator
 from tifffile import tifffile
 
 from refinery.util.java import imglib2, imagej1
@@ -20,42 +21,51 @@ class RefineMode(Enum):
     fit = "fit"
 
 
-def mean_shift_point(swc_point, img, radius, n_iter=10, interval=None):
-    disp = float("inf")
-    x = swc_point.x
-    y = swc_point.y
-    z = swc_point.z
-    cx = x
-    cy = y
-    cz = z
-    rr = radius ** 2
-
-    logging.debug(f"initial point: {[z, y, x]}")
-
-    if interval is not None:
-        gz, gy, gx = np.mgrid[
-                     interval.min(0):interval.max(0),
-                     interval.min(1):interval.max(1),
-                     interval.min(2):interval.max(2)
-                     ]
+def interpolate(img, ival=None):
+    if ival is None:
+        z = np.arange(0, img.shape[0], dtype=int)
+        y = np.arange(0, img.shape[1], dtype=int)
+        x = np.arange(0, img.shape[2], dtype=int)
     else:
-        gz, gy, gx = np.mgrid[
-                     0:img.shape[0],
-                     0:img.shape[1],
-                     0:img.shape[2]
-                     ]
+        # interpolate on the interval that defines the image
+        z = np.arange(ival.min(0), ival.max(0), dtype=int)
+        y = np.arange(ival.min(1), ival.max(1), dtype=int)
+        x = np.arange(ival.min(2), ival.max(2), dtype=int)
+    return RegularGridInterpolator((z, y, x), img)
+
+
+def sphere_coords(x, y, z, r):
+    rr = r ** 2
+    coords = []
+    for dz in np.linspace(z - r, z + r, 2 * r + 1, dtype=float):
+        for dy in np.linspace(y - r, y + r, 2 * r + 1, dtype=float):
+            for dx in np.linspace(x - r, x + r, 2 * r + 1, dtype=float):
+                dd = (dx - x) ** 2 + (dy - y) ** 2 + (dz - z) ** 2
+                if dd <= rr:
+                    coords.append([dz, dy, dx])
+    return np.array(coords)
+
+
+def mean_shift_point(swc_point, img, radius, n_iter=10, interval=None, return_disp=False):
+    cx = swc_point.x
+    cy = swc_point.y
+    cz = swc_point.z
+
+    logging.debug(f"initial point: {[cz, cy, cx]}")
+
+    interpolant = interpolate(img, interval)
 
     count = 0
     displacements = []
+    disp = float("inf")
     while disp >= 0.5 and count < n_iter:
         count += 1
 
         # get locations corresponding to the sphere centered at z,y,x
-        r = (gx - x) ** 2 + (gy - y) ** 2 + (gz - z) ** 2
-        s_idx = np.nonzero(r <= rr)
+        sc = sphere_coords(cx, cy, cz, radius)
 
-        # get the intensity values
-        vals = img[s_idx]
+        # get the interpolated intensity values
+        vals = interpolant(sc)
 
         # compute weights
         w = vals - vals.mean()
@@ -66,31 +76,20 @@ def mean_shift_point(swc_point, img, radius, n_iter=10, interval=None):
             return
         w_pos = w[w_idx]
 
-        # get indices of positive weights in the sphere
-        z_idx = s_idx[0][w_idx]
-        y_idx = s_idx[1][w_idx]
-        x_idx = s_idx[2][w_idx]
-
-        # get coordinates of these points in the original array
-        zs = gz[z_idx, y_idx, x_idx]
-        ys = gy[z_idx, y_idx, x_idx]
-        xs = gx[z_idx, y_idx, x_idx]
+        # get corresponding sphere coordinates
+        c_pos = sc[w_idx]
 
         # add dummy dim for broadcasting purposes
         w_pos = w_pos[..., np.newaxis]
         # compute shifted coordinate
-        c = np.round((np.column_stack((zs, ys, xs)) * w_pos).sum(axis=0) / w_pos.sum())
+        c = (c_pos * w_pos).sum(axis=0) / w_pos.sum()
+
+        disp = np.linalg.norm(c - np.array([cz, cy, cx]))
+        displacements.append(disp)
 
         cz = c[0]
         cy = c[1]
         cx = c[2]
-
-        disp = np.linalg.norm(c - np.array([z, y, x]))
-        displacements.append(disp)
-
-        x = cx
-        y = cy
-        z = cz
 
     swc_point.x = cx
     swc_point.y = cy
@@ -98,7 +97,8 @@ def mean_shift_point(swc_point, img, radius, n_iter=10, interval=None):
 
     logging.debug(f"adjusted: {cz} {cy} {cx}")
 
-    return displacements
+    if return_disp:
+        return displacements
 
 
 def refine_graph(graph, img, radius, n_iter, n_threads=8):
