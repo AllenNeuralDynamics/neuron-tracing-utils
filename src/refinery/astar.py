337aaf502e5976cd918ff465893d16492b574503
Cameron Arshadi <arshadic23@gmail.com>
2023-01-18 14:46:03 -0800

    refactor astar.py

--- src/refinery/astar.py
+++ src/refinery/astar.py
@@ -8,19 +8,24 @@ from pathlib import Path
 import time
 import itertools
 
-from refinery.util import sntutil
+from refinery.util import sntutil, ioutil, imgutil
+from refinery.util.ioutil import ImgReaderFactory
 from refinery.util.java import snt
-from refinery.util.java import imglib2, imagej1, n5
+from refinery.util.java import imglib2, imagej1
 from refinery.transform import WorldToVoxel
 
-import dask.array as da
 import scyjava
 import numpy as np
-import zarr
-import imglyb
 
 
-def astar_graph(in_swc, out_swc, img, calibration):
+def astar_swc(
+        in_swc,
+        out_swc,
+        im_path,
+        calibration,
+        key=None,
+        timeout=60,
+):
     # declare Java classes we will use
     Euclidean = snt.Euclidean
     Reciprocal = snt.Reciprocal
@@ -31,8 +36,11 @@ def astar_graph(in_swc, out_swc, img, calibration):
     Views = imglib2.Views
     DoubleType = imglib2.DoubleType
     ComputeMinMax = imglib2.ComputeMinMax
-    
+
     print(f"processing {in_swc}")
+
+    reader = ImgReaderFactory.create(im_path)
+    img = imgutil.get_hyperslice(reader.load(im_path, key=key))
 
     graph = Tree(in_swc).getGraph()
 
@@ -71,21 +79,6 @@ def astar_graph(in_swc, out_swc, img, calibration):
         minmax = ComputeMinMax(iterable, DoubleType(), DoubleType())
         minmax.process()
 
-        # s = 0
-        # c = 0
-        # mx = float("-inf")
-        # for n in iterable:
-        #     if n is None:
-        #         continue
-        #     val = n.get()
-        #     s += val
-        #     c += 1
-        #     if val > mx:
-        #         mx = val
-        # mean = s / float(c)
-    
-        # print(mean, mx)
-
         # reciprocal of intensity * distance is our cost for moving to a neighboring node
         cost = Reciprocal(
             minmax.getMin().getRealDouble(), minmax.getMax().getRealDouble()
@@ -100,8 +93,8 @@ def astar_graph(in_swc, out_swc, img, calibration):
             tx,
             ty,
             tz,
-            60, # timeout (s)
-            1000, # debug mode reporting interval (ms)
+            timeout,  # timeout (s)
+            -1,  # debug mode reporting interval (ms)
             SNT.SearchImageType.MAP,
             cost,
             heuristic,
@@ -143,39 +136,79 @@ def astar_graph(in_swc, out_swc, img, calibration):
     tree.setSWCType("axon")
     tree.setRadii(1.0)
     tree.saveAsSWC(out_swc)
-    
 
+
+def astar_batch(
+    in_swc_dir,
+    out_swc_dir,
+    im_dir,
+    calibration,
+    key=None,
+    threads=1,
+):
+    im_fmt = ioutil.get_file_format(im_dir)
+
+    in_swcs = []
+    out_swcs = []
+    im_paths = []
+
+    for root, dirs, files in os.walk(in_swc_dir):
+        swcs = [f for f in files if f.endswith(".swc")]
+        if not swcs:
+            continue
+
+        im_path = os.path.join(im_dir, os.path.basename(root) + im_fmt)
+        if not os.path.isfile(im_path):
+            raise FileNotFoundError(f"{im_path} does not exist")
+
+        for f in swcs:
+            in_swc = os.path.join(root, f)
+            in_swcs.append(in_swc)
+            logging.info(f"Running A-star on {in_swc}")
+
+            out_swc = os.path.join(
+                out_swc_dir, os.path.relpath(in_swc, in_swc_dir)
+            )
+            Path(out_swc).parent.mkdir(exist_ok=True, parents=True)
+            out_swcs.append(out_swc)
+
+            im_paths.append(im_path)
+
+    times = len(in_swcs)
+
+    t0 = time.time()
+    with ThreadPoolExecutor(threads) as executor:
+        executor.map(
+            astar_swc,
+            in_swcs,
+            out_swcs,
+            im_paths,
+            itertools.repeat(calibration, times),
+            itertools.repeat(key, times)
+        )
+    t1 = time.time()
+    logging.info(f"processed {times} swcs in {t1-t0}s")
 
 
 def astar_swcs(
     in_swc_dir,
     out_swc_dir,
-    imdir,
+    im_path,
     calibration,
-    swc_type="axon",
-    swc_radius=1.0,
+    key=None,
+    threads=1,
 ):
-    #IJLoader = imglib2.IJLoader
-    Tree = snt.Tree
-
-    #loader = IJLoader()
-
-    s3 = n5.AmazonS3ClientBuilder.defaultClient()
-    reader = n5.N5AmazonS3Reader(s3, "janelia-mouselight-imagery", "carveouts/2018-08-01/fluorescence-near-consensus.n5")
-    img = n5.N5Utils.openWithBoundedSoftRefCache(reader, "volume-rechunked", 5000)
-    print(img.dimensionsAsLongArray())
-    view = imglib2.Views.hyperSlice(img, 3, 0)
+    reader = ImgReaderFactory.create(im_path)
+    view = imgutil.get_hyperslice(reader.load(im_path, key=key))
 
     in_swcs = []
     out_swcs = []
 
     for root, dirs, files in os.walk(in_swc_dir):
         swcs = [f for f in files if f.endswith(".swc")]
+        if not swcs:
+            continue
         for f in swcs:
-            if "consensus" not in f:
-                continue
-            #n5 = os.path.join(imdir, os.path.basename(root) + ".n5")
-
             in_swc = os.path.join(root, f)
             in_swcs.append(in_swc)
             logging.info(f"Running A-star on {in_swc}")
@@ -187,19 +220,18 @@ def astar_swcs(
             out_swcs.append(out_swc)
 
     times = len(in_swcs)
-    
+
     t0 = time.time()
-    with ThreadPoolExecutor(16) as executor:
+    with ThreadPoolExecutor(threads) as executor:
         executor.map(
-            astar_graph, 
+            astar_swc,
             in_swcs,
             out_swcs,
-            itertools.repeat(view, times), 
-            itertools.repeat(calibration, times)
+            itertools.repeat(view, times),
+            itertools.repeat(calibration, times),
         )
     t1 = time.time()
     logging.info(f"processed {times} swcs in {t1-t0}s")
-
 
 
 def main():
@@ -211,9 +243,9 @@ def main():
         "--output", type=str, help="directory to output refined .swc files"
     )
     parser.add_argument(
-        "--images",
+        "--image",
         type=str,
-        help="directory of images associated with the .swc files",
+        help="image or directory of images associated with the .swc files",
     )
     parser.add_argument(
         "--transform", type=str, help='path to the "transform.txt" file'
@@ -221,7 +253,16 @@ def main():
     parser.add_argument(
         "--voxel-size",
         type=str,
-        help="voxel size for images, as a string in XYZ order, e.g., '0.3,0.3,1.0'"
+        help="voxel size for images, as a string in XYZ order, e.g., '0.3,0.3,1.0'",
+    )
+    parser.add_argument(
+        "--dataset", type=str, default=None, help="key for the N5/Zarr dataset"
+    )
+    parser.add_argument(
+        "--threads",
+        type=int,
+        default=1,
+        help="number of threads to use for processing",
     )
     parser.add_argument("--log-level", type=int, default=logging.INFO)
 
@@ -229,8 +270,8 @@ def main():
 
     os.makedirs(args.output, exist_ok=True)
 
-    with open(os.path.join(args.output, 'args.json'), 'w') as f:
-        args.__dict__['script'] = parser.prog
+    with open(os.path.join(args.output, "args.json"), "w") as f:
+        args.__dict__["script"] = parser.prog
         json.dump(args.__dict__, f, indent=2)
 
     logging.basicConfig(format="%(asctime)s %(message)s")
@@ -253,7 +294,26 @@ def main():
     calibration.pixelDepth = voxel_size[2]
 
     logging.info("Starting A-star...")
-    astar_swcs(args.input, args.output, args.images, calibration)
+    if os.path.isdir(args.image):
+        astar_batch(
+            args.input,
+            args.output,
+            args.image,
+            calibration,
+            args.dataset,
+            args.threads,
+        )
+    elif os.path.isfile(args.image):
+        astar_swcs(
+            args.input,
+            args.output,
+            args.image,
+            calibration,
+            args.dataset,
+            args.threads,
+        )
+    else:
+        raise FileNotFoundError(f"{args.image} was not found")
     logging.info("Finished A-star.")
 
 
