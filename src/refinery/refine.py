d5d1c428c533cd16750d1751c3a42f49719ff41f
Cameron Arshadi <arshadic23@gmail.com>
2023-01-19 16:19:11 -0800

    WIP: update refine.py for large volumes

--- src/refinery/refine.py
+++ src/refinery/refine.py
@@ -7,12 +7,18 @@ from concurrent.futures import ThreadPoolExecutor
 from enum import Enum
 from pathlib import Path
 
+import dask.array
 import numpy as np
 import scyjava
+import tifffile
 from scipy.interpolate import RegularGridInterpolator
-from tifffile import tifffile
+from tensorstore import TensorStore
+from tqdm import tqdm
 
-from refinery.util.java import imglib2, imagej1
+from refinery.util import ioutil
+from refinery.util.chunkutil import chunk_center
+from refinery.util.imgutil import get_hyperslice
+from refinery.util.ioutil import ImgReaderFactory, open_ts, open_n5_zarr_as_ndarray
 from refinery.util.java import snt
 
 
@@ -35,7 +41,7 @@ def interpolate(img, ival=None):
 
 
 def sphere_coords(x, y, z, r):
-    rr = r ** 2
+    rr = r**2
     coords = []
     for dz in np.linspace(z - r, z + r, 2 * r + 1, dtype=float):
         for dy in np.linspace(y - r, y + r, 2 * r + 1, dtype=float):
@@ -46,14 +52,48 @@ def sphere_coords(x, y, z, r):
     return np.array(coords)
 
 
-def mean_shift_point(swc_point, img, radius, n_iter=10, interval=None, return_disp=False):
+def mean_shift_point(
+    swc_point,
+    im,
+    radius,
+    n_iter=10,
+    interval=None,
+    return_disp=False,
+    crop_interval=True,
+):
+    if crop_interval:
+        if isinstance(im, dask.array.Array):
+            print(type(im))
+            try:
+                im = im[
+                    interval.min(0) : interval.max(0),
+                    interval.min(1) : interval.max(1),
+                    interval.min(2) : interval.max(2),
+                ].compute()
+            except Exception as e:
+                print(e)
+
+
+        elif isinstance(im, TensorStore):
+            im = im[
+                interval.min(0) : interval.max(0),
+                interval.min(1) : interval.max(1),
+                interval.min(2) : interval.max(2),
+            ].read().result()
+        else:
+            im = im[
+                interval.min(0) : interval.max(0),
+                interval.min(1) : interval.max(1),
+                interval.min(2) : interval.max(2),
+            ]
+
     cx = swc_point.x
     cy = swc_point.y
     cz = swc_point.z
 
-    logging.debug(f"initial point: {[cz, cy, cx]}")
+    logging.info(f"initial point: {[cz, cy, cx]}")
 
-    interpolant = interpolate(img, interval)
+    interpolant = interpolate(im, interval)
 
     count = 0
     displacements = []
@@ -95,23 +135,34 @@ def mean_shift_point(swc_point, img, radius, n_iter=10, interval=None, return_di
     swc_point.y = cy
     swc_point.z = cz
 
-    logging.debug(f"adjusted: {cz} {cy} {cx}")
+    logging.info(f"adjusted: {cz} {cy} {cx}")
 
     if return_disp:
         return displacements
 
 
-def refine_graph(graph, img, radius, n_iter, n_threads=8):
-    vertices = (v for v in graph.vertexSet())
+def refine_graph(
+    graph, img, radius, n_iter, n_threads=8, crop_intervals=False
+):
+    vertices = [v for v in graph.vertexSet()]
     times = graph.vertexSet().size()
+    if crop_intervals:
+        ivals = []
+        for v in vertices:
+            ivals.append(chunk_center(np.array([v.z, v.y, v.x]), [128, 128, 128]))
+    else:
+        ivals = [None] * times
     with ThreadPoolExecutor(max_workers=n_threads) as executor:
-        executor.map(
-            mean_shift_point,
-            vertices,
-            itertools.repeat(img, times),
-            itertools.repeat(radius, times),
-            itertools.repeat(n_iter, times)
-        )
+        futures = []
+        for i in range(len(vertices)):
+            futures.append(
+                executor.submit(mean_shift_point, vertices[i], img, radius, n_iter, ivals[i], False, crop_intervals)
+            )
+        for fut in tqdm(futures):
+            try:
+                fut.result()
+            except Exception as e:
+                print(e)
 
 
 def fit_tree(tree, img, radius=1):
@@ -124,11 +175,69 @@ def fit_tree(tree, img, radius=1):
         fitter.call()
 
 
-def refine_swcs(
-        in_swc_dir, out_swc_dir, imdir, radius=1, mode=RefineMode.mean_shift.value, threads=1
+def refine_swcs_batch(
+    in_swc_dir,
+    out_swc_dir,
+    im_dir,
+    radius=1,
+    mode=RefineMode.mean_shift.value,
+    threads=1,
+    key=None,
+    mean_shift_iter=1,
 ):
-    loader = imglib2.IJLoader()
-    IJ = imagej1.IJ
+    im_fmt = ioutil.get_file_format(im_dir)
+    for root, dirs, files in os.walk(in_swc_dir):
+        swcs = [f for f in files if f.endswith(".swc")]
+        if not swcs:
+            continue
+
+        im_path = os.path.join(im_dir, os.path.basename(root) + im_fmt)
+
+        for f in swcs:
+            swc_path = os.path.join(root, f)
+            logging.info(f"Refining {swc_path}")
+
+            out_swc = os.path.join(
+                out_swc_dir, os.path.relpath(swc_path, in_swc_dir)
+            )
+            Path(out_swc).parent.mkdir(exist_ok=True, parents=True)
+
+            tree = snt.Tree(swc_path)
+
+            if mode == RefineMode.mean_shift.value:
+                # FIXME support more formats for Python
+                im = tifffile.imread(im_path)
+                graph = tree.getGraph()
+                refine_graph(
+                    graph,
+                    im,
+                    radius,
+                    n_iter=mean_shift_iter,
+                    n_threads=threads,
+                    crop_intervals=False,
+                )
+                graph.getTree().saveAsSWC(out_swc)
+            elif mode == RefineMode.fit.value:
+                img = get_hyperslice(
+                    ImgReaderFactory.create(im_path).load(im_path, key=key),
+                    ndim=3,
+                )
+                fit_tree(tree, img, radius=radius)
+                tree.saveAsSWC(out_swc)
+            else:
+                raise ValueError(f"Invalid mode {mode}")
+
+
+def refine_swcs(
+    in_swc_dir,
+    out_swc_dir,
+    im_path,
+    radius=1,
+    mode=RefineMode.mean_shift.value,
+    threads=1,
+    key=None,
+    mean_shift_iter=1,
+):
     for root, dirs, files in os.walk(in_swc_dir):
         swcs = [f for f in files if f.endswith(".swc")]
         for f in swcs:
@@ -143,19 +252,23 @@ def refine_swcs(
             tree = snt.Tree(swc_path)
 
             if mode == RefineMode.mean_shift.value:
-                tif = os.path.join(imdir, os.path.basename(root) + ".tif")
-                img = tifffile.imread(tif)
+                im = open_ts(im_path, dataset=key, total_bytes_limit=500_000_000)
+                while im.ndim > 3:
+                    im = im[0, ...]
                 graph = tree.getGraph()
-                refine_graph(graph, img, radius, n_iter=5, n_threads=threads)
+                refine_graph(
+                    graph,
+                    im,
+                    radius,
+                    n_iter=mean_shift_iter,
+                    n_threads=threads,
+                    crop_intervals=True,
+                )
                 graph.getTree().saveAsSWC(out_swc)
             elif mode == RefineMode.fit.value:
-                # Versions prior to 4.04 only accept ImagePlus inputs
-                # this will be orders of magnitude slower than it should be
-                # until SNT versions >= 4.0.4 are available on the scijava maven repository
-                imp = IJ.openImage(
-                    os.path.join(imdir, os.path.basename(root) + ".tif")
-                )
-                fit_tree(tree, imp)
+                reader = ImgReaderFactory.create(im_path)
+                img = get_hyperslice(reader.load(im_path, key=key), ndim=3)
+                fit_tree(tree, img, radius=radius)
                 tree.saveAsSWC(out_swc)
             else:
                 raise ValueError(f"Invalid mode {mode}")
@@ -166,20 +279,26 @@ def main():
     parser.add_argument(
         "--input",
         type=str,
-        default=r"C:\Users\cameron.arshadi\Desktop\repos\20210812-AG-training-data\aligned\swcs\block_3",
+        default=r"C:\Users\cameron.arshadi\Desktop\repos\exaSpim-training-data\exaSPIM_609281_2022-11-03_13-49-18"
+        r"\whole-brain\swcs-transformed",
         help="directory of .swc files to refine",
     )
     parser.add_argument(
         "--output",
         type=str,
-        default=r"C:\Users\cameron.arshadi\Desktop\repos\20210812-AG-training-data\aligned\swcs\block_3\refined",
+        default=r"C:\Users\cameron.arshadi\Desktop\repos\exaSpim-training-data\exaSPIM_609281_2022-11-03_13-49-18"
+        r"\whole-brain\swcs-refined",
         help="directory to output refined .swc files",
     )
     parser.add_argument(
-        "--images",
-        default=r"C:\Users\cameron.arshadi\Desktop\repos\20210812-AG-training-data\aligned\blocks\all",
+        "--image",
+        default=r"https://aind-open-data.s3.amazonaws.com/exaSPIM_609281_2022-11-03_13-49-18_stitched_2022-11-22_12"
+        r"-07-00/fused.zarr/fused.zarr",
         type=str,
-        help="directory of images associated with the .swc files",
+        help="image or directory of images associated with the .swc files",
+    )
+    parser.add_argument(
+        "--dataset", type=str, default="0", help="key for the N5/Zarr dataset"
     )
     parser.add_argument(
         "--mode",
@@ -193,11 +312,8 @@ def main():
         default=2,
         help="search radius for point refinement",
     )
-    parser.add_argument(
-        "--threads",
-        type=int,
-        default=1
-    )
+    parser.add_argument("--mean-shift-iter", type=int, default=3)
+    parser.add_argument("--threads", type=int, default=8)
     parser.add_argument("--log-level", type=int, default=logging.INFO)
 
     args = parser.parse_args()
@@ -214,7 +330,28 @@ def main():
     scyjava.start_jvm()
 
     logging.info("Starting refinement...")
-    refine_swcs(args.input, args.output, args.images, args.radius, args.mode, args.threads)
+    if os.path.isdir(args.image):
+        refine_swcs_batch(
+            args.input,
+            args.output,
+            args.image,
+            args.radius,
+            args.mode,
+            args.threads,
+            args.dataset,
+            args.mean_shift_iter,
+        )
+    else:
+        refine_swcs(
+            args.input,
+            args.output,
+            args.image,
+            args.radius,
+            args.mode,
+            args.threads,
+            args.dataset,
+            args.mean_shift_iter,
+        )
     logging.info("Finished refinement.")
 
 
