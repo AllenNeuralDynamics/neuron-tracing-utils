75b1195fdd69c93622fb61879389978a3e0695ce
Cameron Arshadi <arshadic23@gmail.com>
2023-02-03 14:57:26 -0800

    feat: use latest mobie-io

--- src/refinery/util/java/config.py
+++ src/refinery/util/java/config.py
@@ -6,7 +6,7 @@ _snt_version = os.getenv("SNT_VERSION", _default_snt_version)
 _default_fiji_version = "2.9.0"
 _fiji_version = os.getenv("FIJI_VERSION", _default_fiji_version)
 
-_default_mobie_io_version = "2.0.7"
+_default_mobie_io_version = "2.0.10"
 _mobie_io_version = os.getenv("MOBIE_IO_VERSION", _default_mobie_io_version)
 
 _default_java_opts = None
