6445b8132ba8a9426483f7b034cdea01a1cf2fbe
Cameron Arshadi <cameron.arshadi@alleninstitute.org>
2026-03-28 03:27:36 +0000

    astar.py: fix swc types

--- src/neuron_tracing_utils/astar.py
+++ src/neuron_tracing_utils/astar.py
@@ -168,7 +168,8 @@ def astar_swc(
         reader = ImgReaderFactory.create(img)
         img = imgutil.get_hyperslice(reader.load(img, key=key, cache=cache))
 
-    graph = snt.Tree(in_swc).getGraph()
+    source_tree = snt.Tree(in_swc)
+    graph = source_tree.getGraph()
     # remove duplicates up front
     dups = prune_contiguous_dups(graph)
     print(f"removed {len(dups)} dups")
@@ -205,22 +206,28 @@ def astar_swc(
 
         graph.removeEdge(edge)
         tmp = graph.addVertex(path_arr[0][0], path_arr[0][1], path_arr[0][2])
+        # New graph vertices default to type 0, so inherit the child type
+        # to preserve the original SWC labels along this subdivided edge.
+        tmp.type = edge.getTarget().type
         graph.addEdge(edge.getSource(), tmp)
         prev = tmp
         for i in range(1, len(path_arr)):
             tmp = graph.addVertex(
                 path_arr[i][0], path_arr[i][1], path_arr[i][2]
             )
+            tmp.type = edge.getTarget().type
             graph.addEdge(prev, tmp)
             prev = tmp
         graph.addEdge(tmp, edge.getTarget())
 
     prune_contiguous_dups(graph)
+    graph.updateVertexProperties()
 
-    tree = graph.getTree()
+    # Rebuild from the typed SWCPoint vertices because graph.getTree()
+    # flattens mixed original SWC types during the graph-to-tree round-trip.
+    tree = snt.Tree(graph.vertexSet(), source_tree.getLabel())
     # Set a non-zero radius.
     # Some programs (JWS) fail to import .swc files with radii == 0
-    tree.setSWCType("axon")
     tree.setRadii(1.0)
     tree.saveAsSWC(out_swc)
 
